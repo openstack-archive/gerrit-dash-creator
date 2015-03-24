ae148db10adcb523349aaab48f2a97d1aeff778f
Angus Salkeld <asalkeld@mirantis.com>
2015-03-24 15:27:53 +1000

    Automatically generate a dash based on "In Progress" bugs
    This adds a script to get the "In Progress" bugs for a
    particular project and builds prints a URL for the user.
    
     $ ./gerrit-bug-dash --milestone juno-rc1 heat ceilometer
    
    Some notes:
     - it uses launchpadlib to talk to launchpad
     - The presentation of the dashboard could be improved but
       this is just a starting point.
     - The caching could be better.
    
    What I wanted was to know was "what do I need to review for juno-rc1"
    (i.e. what are the bugs that are targeted for juno-rc1 that are in
    progress and have reviews up).
    
    This scratches my itch, but will happily do some work on it if others
    want it.
    
    Change-Id: I4a97d59631ac9cd344206c6cc48164d6a0d7e57c

---
+++ gerrit_dash_creator/cmd/bugs.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import argparse
+import os
+from six.moves import configparser
+import sys
+
+from launchpadlib import launchpad
+
+from gerrit_dash_creator.cmd import creator
+
+CACHE_DIR = os.path.expanduser('~/.cache/launchpadlib/')
+SERVICE_ROOT = 'production'
+
+
+def print_dash_url(projects, bugs):
+    config = configparser.ConfigParser()
+    config.add_section('dashboard')
+    config.set('dashboard', 'title',
+               'Priortized Bug Fix Dashboard')
+    config.set('dashboard', 'description', 'Bug Fix Inbox')
+
+    proj_q = ['project:openstack/%s' % proj for proj in projects]
+    config.set('dashboard', 'foreach',
+               '(%s) status:open ' % ' OR '.join(proj_q))
+
+    for milestone in bugs:
+        for prio in bugs[milestone]:
+            if len(bugs[milestone][prio]) == 0:
+                continue
+            sect = 'section "Milestone %s Importance %s"' % (milestone, prio)
+            config.add_section(sect)
+            config.set(sect, 'query',
+                       ' OR '.join(['change:%s' % bug
+                                    for bug in bugs[milestone][prio]]))
+
+    print(creator.generate_dashboard_url(config))
+
+
+def pretty_milestone(milestone_url):
+    if milestone_url is None:
+        return 'all'
+    # https://api.launchpad.net/1.0/heat/+milestone/next:
+    return str(milestone_url).split('/')[-1]
+
+
+def review_id_from_bug(bug):
+    for msg in bug.bug.messages:
+        if 'Fix proposed' in msg.subject:
+            for line in str(msg.content).split('\n'):
+                if 'Review' in line:
+                    return line.split('/')[-1]
+
+
+def get_options():
+    """Parse command line arguments and options."""
+    parser = argparse.ArgumentParser(
+        description='Create a Gerrit dashboard URL from launchpad '
+                    '"In Progress bugs')
+    parser.add_argument('projects', nargs='+',
+                        metavar='projects',
+                        help='Launchpad Projects')
+    parser.add_argument('--milestone', default=None,
+                        help='Project Milestone')
+    return parser.parse_args()
+
+
+def process_project(lp, opts, project_name, bugs):
+    project = lp.projects[project_name]
+    review_bugtasks = project.searchTasks(status=['In Progress'])
+
+    for bug in review_bugtasks:
+        milestone = pretty_milestone(bug.milestone)
+        if opts.milestone is not None and milestone != opts.milestone:
+            continue
+
+        if milestone not in bugs:
+            bugs[milestone] = {}
+
+        if bug.importance not in bugs[milestone]:
+            bugs[milestone][bug.importance] = []
+
+        rev_no = review_id_from_bug(bug)
+        if rev_no is not None:
+            bugs[milestone][bug.importance].append(rev_no)
+            print('[%s] %s -> %s' % (bug.importance,
+                                     bug, rev_no))
+
+
+def main():
+    """Entrypoint."""
+
+    opts = get_options()
+    lpad = launchpad.Launchpad.login_anonymously(sys.argv[0],
+                                                 SERVICE_ROOT,
+                                                 CACHE_DIR)
+    bugs = {}
+    for proj in opts.projects:
+        process_project(lpad, opts, proj, bugs)
+
+    print('')
+    print_dash_url(opts.projects, bugs)
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
