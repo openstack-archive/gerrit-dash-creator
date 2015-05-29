6e1493493c3ec549dd75ee8ec883056c90150b00
John L. Villalovos <john.l.villalovos@intel.com>
2015-05-29 14:44:00 -0700

    Add ability to dynamically generate the foreach projects
    Add support for being able to use {{projects}} in the foreach line.  If
    that is specified and 'project' is set to the project name (e.g. Ironic)
    then the project governance file will be downloaded and used to
    determine all the project repositories for the project.
    
    Also update dashboards/ironic.dash to use this feature.
    
    Change-Id: I8b9e5e416818a8b4599d4964922fe008796124d7

--- gerrit_dash_creator/cmd/creator.py
+++ gerrit_dash_creator/cmd/creator.py
@@ -20,8 +20,17 @@ import urllib
 
 import jinja2
 import pkg_resources
+import requests
 import six
 from six.moves import configparser
+import yaml
+
+
+PROJECTS_URL = ("https://git.openstack.org/cgit/openstack/governance/plain/"
+                "reference/projects.yaml")
+# Used to store the dictionary of the processed PROJECTS_URL.  Only retrieved
+# if needed.
+PROJECTS = None
 
 
 def escape(buff):
@@ -31,6 +40,13 @@ def escape(buff):
 
 def generate_dashboard_url(dashboard):
     """Generate a dashboard URL from a given definition."""
+    try:
+        project = dashboard.get('dashboard', 'project')
+        projects = get_project_repos(project)
+        projects = ' OR '.join(sorted(projects))
+    except configparser.NoOptionError:
+        project = None
+
     try:
         title = dashboard.get('dashboard', 'title')
     except configparser.NoOptionError:
@@ -45,6 +61,10 @@ def generate_dashboard_url(dashboard):
         baseurl = dashboard.get('dashboard', 'baseurl')
     except configparser.NoOptionError:
         baseurl = 'https://review.openstack.org/#/dashboard/?'
+
+    if project:
+        template = jinja2.Template(foreach)
+        foreach = template.render(projects=projects)
 
     url = baseurl
     url += urllib.urlencode({'title': escape(title),
@@ -175,6 +195,30 @@ def load_dashboards(paths):
                                  "parsed: %s" % (dashboard_file, e))
 
     return dashboards
+
+
+def get_projects():
+    """Get the list of official OpenStack projects."""
+
+    # FIXME(jlvilla): Remove when a better way is found
+    requests.packages.urllib3.disable_warnings()
+    result = requests.request('get', PROJECTS_URL)
+    result.raise_for_status()
+    projects = yaml.load(result.text)
+    return projects
+
+
+def get_project_repos(project):
+    """Get all the project repos for an OpenStack project."""
+
+    global PROJECTS
+    if PROJECTS is None:
+        PROJECTS = get_projects()
+    projects = PROJECTS[project]['projects']
+    project_repos = []
+    for project_data in projects:
+        project_repos.append('project:{}'.format(project_data['repo']))
+    return project_repos
 
 
 def main():
